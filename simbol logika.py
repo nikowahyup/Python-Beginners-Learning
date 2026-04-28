7356e8098803f1820ae6a86e7fc41bdd421f7e1f
nikowahyup <nikowahyupratama@gmail.com>
2026-04-28 14:09:02 +0700

    simbol logika matematika

---
+++ simbol logika.py
@@ -0,0 +1,32 @@
+# Simbol logika adalah klasifikasi sendiri dari operator matematika
+# Contohnya seperti kurang dari,lebih dari dll
+# (>) Ini adalah simbol lebih dari
+# (<) Ini adalah simbol kurang dari
+# (>=) Ini adalah simbol lebih dari atau sama dengan
+# (<=) Ini adalah simbol kurang dari atau sama dengan
+# (==) Ini adalah simbol sama dengan
+# (!=) Ini adalah simbol tidak sama dengan
+
+# Operator-operator ini nantinya akan mengeluarkan output data boolean
+
+
+# contoh
+
+a = 1
+b = 2
+c = 5
+d = 10
+
+# Pengecekan
+
+print(a > b) # Ini akan bernilai False
+print(a == b) # Ini juga False
+print(a != b ) # Ini akan bernilai True
+
+# Selain pengecekan seperti itu,kita juga bisa menyimpan hasil di dalam variabel
+
+
+x = a == b
+print(x)
+
+
