a1b89773f9861dcedf028e96e0c6d8810385c379
nikowahyup <nikowahyupratama@gmail.com>
2026-04-28 11:53:57 +0700

    Pengenalan variabel

---
+++ 01 dasar/variabel.py
@@ -0,0 +1,19 @@
+# Variabel adalah tempat penyimpanan data. Jika dalam matematika kita sering menggunakan variabel x untuk memisalkan sebuah nilai.
+# Sama seperti variabel dalam matematika,variabel di pemrograman bisa untuk menyimpan nilai/data
+
+# Contoh membuat variabel dan menyimpan suatu data
+
+nama = "Niko Wahyu Pratama" # variabel "nama" menyimpan data string "Niko Wahyu Pratama"
+usia = 18 # Variabel "usia" menyimpan data integer bernilai 18
+
+# Nantinya variabel-variabel ini bisa digunakan dalam kode selanjutnya,contohnya:
+#print perkenalan menggunakan format-string/f-string
+
+print(f"nama saya {nama}. Usia saya sekarang {usia} tahun")
+
+
+
+
+
+
+
