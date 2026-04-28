cae508def91e687c350875e9b236ef9b8fabd392
nikowahyup <nikowahyupratama@gmail.com>
2026-04-28 11:52:56 +0700

    Pengenalan operasi matematika

---
+++ 01 dasar/Aritmatika.py
@@ -0,0 +1,43 @@
+# Untuk mengolah data seperti dalam matematika,di Python juga bisa menggunakan operasi matematika
+# Simbol (+) untuk melakukan penjumlahan
+# Simbol (-) untuk melakukan pengurangan
+# Simbol (*) untuk melakukan perkalian
+# Simbol (/) untuk melakukan pembagian(hasil selalu berbentuk floating)
+# Simbol (**) untuk melakukan pemangkatan
+# Simbol (%) untuk melihat sisa pembagian(modulo)
+# Simbol (//) untuk melihat hasil pembagian bulat
+# Aturan P.E.M.D.A.S juga tetap berlaku
+
+# Contoh penggunaan
+
+a = 10
+b = 6
+c = 100
+d = 5
+
+print(a + c) # Hasilnya 110
+print(a * d) # Hasilnya 50
+print(c / d ) # Hasilnya 20.0
+print(a % b) # Hasilnya 4
+print(a // b) # Hasilnya 1
+print(c - b) # Hasilnya 94
+print(a**2) # Hasilnya 100
+
+# Bisa juga hasil operasi disimpan di dalam variabel lain
+
+x = c % b
+print(x) # Hasilnya 4
+
+
+# Bisa juga tanda operasi diletakkan sebelum tanda assigment (=)
+
+a += b
+print(a) # Ini akan mengeluarkan output yang sama seperti a + b,yaitu 16
+
+a %= d
+print(a) # Hasilnya 1
+
+
+
+
+
