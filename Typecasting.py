3bfc72f13ec0296cb5f99f5b5a4ce1d5615b85fe
nikowahyup <nikowahyupratama@gmail.com>
2026-04-29 11:34:16 +0700

    Mengubah tipe data

---
+++ Typecasting.py
@@ -0,0 +1,24 @@
+# Type casting adalah cara mengubah tipe data
+# str(),int(),float(),bool()
+
+# CONTOH PENGGUNAAAN
+
+nama = "Niko" # ini adalah tipe data string
+print(type(nama))
+usia = 18 # ini adalah tipe data integer
+print(type(usia))
+ipk = 3.5 # ini adalah tipe data floating
+print(type(ipk))
+lulus = True # ini adalah tipe data boolean
+print(type(lulus))
+
+# MENGUBAH TIPE DATA
+
+ipk = int(ipk)
+print(type(ipk)) # Awalnya ipk bertipe floating,setelah diubah menjadi integer
+
+
+
+
+
+
